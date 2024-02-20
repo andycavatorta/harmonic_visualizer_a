cdba2ac94d47369e62b30101a837fd71f02bdc81
Andy Cavatorta <andycavatorta@gmail.com>
2024-02-19 20:39:41 -0500

    prep to test fpga

--- amt203s.py
+++ amt203s.py
@@ -86,7 +86,7 @@ class AMT203s(threading.Thread):
         ):
         threading.Thread.__init__(self)
         self.delay_sec = delay #/ 1E3
-        self.delay_usec = delay / 1E3
+        self.delay_usec = 0.01
         self.polling_period = polling_period
         self.event_receiver = event_receiver
 
