f812edd942ea84b0ec6cdabea12bb897750c0c5c
Andy Cavatorta <andycavatorta@gmail.com>
2024-02-20 19:13:02 -0500

    prep to test fpga

--- main.py
+++ main.py
@@ -149,13 +149,15 @@ class Signals():
         100 fffff (frequency)
         101 fffff (frequency)
         """
+        last_continuity_for_debug = dict(self.continuity_for_debug)
         self.continuity_for_debug[channel_str] = frequency
-        print(
-            self.continuity_for_debug["01"],
-            self.continuity_for_debug["02"],
-            self.continuity_for_debug["03"],
-            self.continuity_for_debug["04"]
-        )
+        if last_continuity_for_debug != self.continuity_for_debug:
+            print(
+                self.continuity_for_debug["01"],
+                self.continuity_for_debug["02"],
+                self.continuity_for_debug["03"],
+                self.continuity_for_debug["04"]
+            )
         channel_id_binary_str = self.decimal_to_binary_string(int(channel_str)+1, 6)
         if frequency == 0:
             period_binary_str = "000000000000000000000000"
