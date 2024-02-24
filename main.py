11b73d9cef68a2e2c9de91fcc68d3ef72e9f3150
Andy Cavatorta <andycavatorta@gmail.com>
2024-02-24 17:18:26 -0500

    prep to test fpga

--- main.py
+++ main.py
@@ -91,10 +91,10 @@ class Signals():
     to do: automatic reconnect
     """
     names_to_fpga__gpio_numbers = {
-        "a":"00",
-        "b":"02",
-        "c":"04",
-        "d":"06",
+        "a":"02",
+        "b":"04",
+        "c":"06",
+        "d":"08",
     }
 
     def __init__(
@@ -108,10 +108,10 @@ class Signals():
             "is_open":False
         }
         self.continuity_for_debug = {
-            "00":0,
             "02":0,
             "04":0,
             "06":0,
+            "08":0,
         }
         self.connect()
 
@@ -154,10 +154,10 @@ class Signals():
         if last_continuity_for_debug == self.continuity_for_debug:
             return
         print(
-            self.continuity_for_debug["00"],
             self.continuity_for_debug["02"],
             self.continuity_for_debug["04"],
             self.continuity_for_debug["06"],
+            self.continuity_for_debug["08"],
         )
         channel_id_binary_str = self.decimal_to_binary_string(int(channel_str)+1, 6)
         if frequency == 0:
